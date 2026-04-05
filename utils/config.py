e6ba35759d686eec889f451696d88f414a879f79
shrijal10333 <armn9548@gmail.com>
2026-04-05 05:19:44 -0700

    first upload

---
+++ utils/config.py
@@ -0,0 +1,25 @@
+"""
+: ! Aegis !
+    + Discord: itsfizys
+    + Community: https://discord.gg/aerox (AeroX Development )
+    + for any queries reach out Community or DM me.
+"""
+import os 
+
+TOKEN =os .environ .get ("TOKEN")
+NAME ="Samaksh-bot"
+server ="http://dsc.gg/eclipseeeee"
+ch ="https://discord.com/channels/1324668335069331477/1324668336470102143"
+OWNER_IDS = [int(x.strip()) for x in os.environ.get("OWNER_IDS", "").split(",") if x.strip()]
+BotName ="Yuna-bot"
+serverLink ="https://discord.gg/JxCFmz9nZP"
+
+invite_link = "https://discord.com/oauth2/authorize?client_id=1488692450686926938"
+website_link = ""
+support_link = "http://dsc.gg/eclipseeeee"
+"""
+: ! Aegis !
+    + Discord: itsfizys
+    + Community:http://dsc.gg/eclipseeeee(AeroX Development )
+    + for any queries reach out Community or DM me.
+"""
